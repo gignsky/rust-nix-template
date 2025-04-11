b047aae96f4d36612e17d80ff7de1299482649b4
gignsky <gig@gignsky.com>
2025-04-11 18:02:38 -0400

    got devshell working for the time being

--- nix/modules/devshell.nix
+++ nix/modules/devshell.nix
@@ -9,14 +9,18 @@
         config.pre-commit.devShell # See ./nix/modules/pre-commit.nix
       ];
       packages = with pkgs; [
-        just  
+        just
         nixd # Nix language server  
-        bacon  
-        config.process-compose.cargo-doc-live.outputs.package  
-        nil  
-        lolcat  
+        bacon
+        # config.process-compose.cargo-doc-live.outputs.package  
+        nil
+        lolcat
         wslu
+        #add quickresults one day
       ];
+      shellHook = ''
+        echo "welcome to the rust development environment" | lolcat
+      '';
     };
   };
 }
